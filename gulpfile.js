b51965a78fd4baee6624b78dfcece60c22715511
wangshijiang <wangshijiang@eqxiu.com>
2016-05-09 10:55:48 +0800

    Add gulp-traceur support

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var traceur = require('gulp-traceur');
 var del = require('del');
 
 gulp.task('clean', () => {
@@ -11,5 +12,11 @@ gulp.task('babel', gulp.series('clean', () => {
         .pipe(babel({
             presets: ['es2015']
         }))
+        .pipe(gulp.dest('dist'));
+}));
+
+gulp.task('traceur', gulp.series('clean', () => {
+    return gulp.src('src/*.js')
+        .pipe(traceur())
         .pipe(gulp.dest('dist'));
 }));
