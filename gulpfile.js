0a09a8fe3077b73a2ce410b0bd8478009b5e1010
wangshijiang <wangshijiang@eqxiu.com>
2016-05-09 10:52:29 +0800

    Using gulp 4.0, add del task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var del = require('del');
 
-gulp.task('default', () => {
+gulp.task('clean', () => {
+    return del('dist');
+});
+
+gulp.task('babel', gulp.series('clean', () => {
     return gulp.src('src/*.js')
-    .pipe(babel({
+        .pipe(babel({
             presets: ['es2015']
         }))
-    .pipe(gulp.dest('dist'));
-});
+        .pipe(gulp.dest('dist'));
+}));
