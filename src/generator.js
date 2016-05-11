146f83f80565d462484f983301f22abd72a77ccb
wangshijiang <wangshijiang@eqxiu.com>
2016-05-11 08:19:49 +0800

    添加斐波那契数列例子

--- src/generator.js
+++ src/generator.js
@@ -12,3 +12,29 @@ console.info(action.next());
 console.info(action.next());
 console.info(action.next());
 console.info(action.next());
+
+
+function* fab(max) {
+    var count = 0, last = 0, current = 1;
+
+    while(max > count++) {
+        yield current;
+        var tmp = current;
+        current += last;
+        last = tmp;
+    }
+}
+
+var o = fab(10), ret, result = [];
+
+while(!(ret = o.next()).done) {
+    result.push(ret.value);
+}
+
+console.log(result); // [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+console.info([...fab(10)]);
+
+for(let current of fab(10)) {
+    console.info(current);
+}
